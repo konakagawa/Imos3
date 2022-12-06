e0dfab20d29cb9f637ef7fe853a8c686051a67cc
Koji Nakagawa <ko.nakagawa@infocom.co.jp>
2022-12-06 16:07:16 +0900

    Update MainTest.java

--- srctest/MainTest.java
+++ srctest/MainTest.java
@@ -39,91 +39,16 @@ public class MainTest {
 	@Test
 	public void test1() throws IOException {
 
-	in.inputln("09012345678");
+		in.inputln("2");
+		in.inputln("1 1 1 4 4 4");
+		in.inputln("2 2 2 5 5 5");
 
+		Main.main(null);
+		StringBuffer sb = new StringBuffer();
 
+		sb.append("4");
 
-
-
-
-
-
-
-
-
-	Main.main(null);
-	StringBuffer sb = new StringBuffer();
-
-	sb.append("819012345678");
-
-	assertEquals(sb.toString(), outContent.toString());
+		assertEquals(sb.toString(), outContent.toString());
 	}
 
-	@Test
-	public void test2() throws IOException {
-
-	in.inputln("09000000000");
-
-
-
-
-
-
-
-
-
-
-
-	Main.main(null);
-	StringBuffer sb = new StringBuffer();
-
-	sb.append("819000000000");
-
-	assertEquals(sb.toString(), outContent.toString());
-	}
-
-
-	/*
-		@Test
-		public void test2() throws IOException {
-			in.inputln("8 9");
-			in.inputln("3 1 4 1 5 9 2 6");
-
-			Main.main(null);
-
-			StringBuffer sb = new StringBuffer();
-
-			sb.append("0");
-			assertEquals(sb.toString(), outContent.toString());
-
-		}
-
-		@Test
-		public void test3() throws IOException {
-			in.inputln("10 314");
-			in.inputln("159 265 358 979 323 846 264 338 327 950");
-
-			Main.main(null);
-
-			StringBuffer sb = new StringBuffer();
-
-			sb.append("1923");
-			assertEquals(sb.toString(), outContent.toString());
-
-		}
-
-		@Test
-		public void test4() throws IOException {
-			in.inputln("200000 0");
-
-			in.inputln(str);
-			Main.main(null);
-
-			StringBuffer sb2 = new StringBuffer();
-
-			sb2.append("20000000000");
-			assertEquals(sb2.toString(), outContent.toString());
-
-		}
-	*/
 }
